a0cd81fc9661ecda4c926f3f1abcaafc1d78f455
Aditya Prakash <prakashaditya2709@gmail.com>
2025-09-06 05:24:49 +0530

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,5 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Hello")
+    println("Welcome to Kotlin!")
+
+    val name: String = "Aditya"
+    val age: Int = 20
+
+    println("Name: $name")
+    println("Age: $age")
+
+    if (age >= 18) {
+        println("$name is an Adult ")
+    } else {
+        println("$name is a Minor ")
+    }
+
+    println("\nCounting from 1 to 5:")
+    for (i in 1..5) {
+        println(i)
+    }
+
+    fun greet(user: String): String {
+        return "Hello, $user! 👋"
+    }
+
+    println("\n" + greet(name))
 }
